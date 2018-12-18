e0ba7e4faa18c865a8fa4d40c7e37e3f2cef79ed
Antonio Rios <antonio.rios.jr@gmail.com>
2018-12-17 21:51:21 -0700

    First program of the courses creating a GUI window.

---
+++ CreateWindow.py
@@ -0,0 +1,11 @@
+#The tkinter module needs to be imported
+import tkinter as tk
+
+# We need an instance of Tk 
+win = tk.Tk()
+
+# We can set the title of the window using the 'title' method
+win.title('Creating a Window GUI With Python')
+
+#call the mainloop method to manage the event queue and render the new window
+win.mainloop()
